c04312bc0e3f5a09cf754150d0119ab91cfe96be
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-03-05 22:01:43 +0100

    STAI scoring r script updated

--- STAI_scoring.R
+++ STAI_scoring.R
@@ -2,29 +2,61 @@
 setwd("~/Documents/GitHub/anxiety_cognitive_maps_analyses")
 library(tidyverse)
 library(dplyr)
+library(tibble)
 
-#set up data frame
-df <- data.frame(Q1 = c(1,2,3,4),
-                 Q2 = c(1,2,3,4),
-                 Q3 = c(2,5,4,3),
-                 Q4 = c(1,2,4,5),
-                 Q5 = c(2,4,5,6))
-df
+#path references
+data_path <- '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/Pilot1/STAIscores.csv'
 
-#reverse score data frame
-reverse_questions = c("Q1", "Q3")
-max_score = 4
-max_num = (4 + 1)
-df[ , reverse_questions] = max_num - df[ , reverse_questions]
-df
+#load data
+df_pilot1 <- read_csv(data_path)
+
+#manipulate df to get it to look how you want
+colnames(df_pilot1) <- c('not_included', 'subjectID', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20')
+num_rows = nrow(df_pilot1)
+num_rows
+
+df_pilot1 <- df_pilot1 %>%
+  slice(2:num_rows) %>%
+  select('subjectID', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20')
+
+#getting scores into numeric form so you can add them together later
+print(sapply(df_pilot1, class)) #debugging
+df_pilot1 <- transform(df_pilot1,s1 = as.numeric(s1))
+print(sapply(df_pilot1$s1, class)) #debugging
+df_pilot1 <- transform(df_pilot1,s2 = as.numeric(s2))
+df_pilot1 <- transform(df_pilot1,s3 = as.numeric(s3))
+df_pilot1 <- transform(df_pilot1,s4 = as.numeric(s4))
+df_pilot1 <- transform(df_pilot1,s5 = as.numeric(s5))
+df_pilot1 <- transform(df_pilot1,s6 = as.numeric(s6))
+df_pilot1 <- transform(df_pilot1,s7 = as.numeric(s7))
+df_pilot1 <- transform(df_pilot1,s8 = as.numeric(s8))
+df_pilot1 <- transform(df_pilot1,s9 = as.numeric(s9))
+df_pilot1 <- transform(df_pilot1,s10 = as.numeric(s10))
+df_pilot1 <- transform(df_pilot1,s11 = as.numeric(s11))
+df_pilot1 <- transform(df_pilot1,s12 = as.numeric(s12))
+df_pilot1 <- transform(df_pilot1,s13 = as.numeric(s13))
+df_pilot1 <- transform(df_pilot1,s14 = as.numeric(s14))
+df_pilot1 <- transform(df_pilot1,s15 = as.numeric(s15))
+df_pilot1 <- transform(df_pilot1,s16 = as.numeric(s16))
+df_pilot1 <- transform(df_pilot1,s17 = as.numeric(s17))
+df_pilot1 <- transform(df_pilot1,s18 = as.numeric(s18))
+df_pilot1 <- transform(df_pilot1,s19 = as.numeric(s19))
+df_pilot1 <- transform(df_pilot1,s20 = as.numeric(s20))
+
+#sum STAI score
+df_pilot1 <- as_tibble(df_pilot1) %>% 
+  group_by(subjectID) %>%
+  mutate(sumVals = (5-s1) + s2 + (5-s3) + s4 + s5 + (5-s6) + (5-s7) + s8 + s9 + (5-s10) + s11 + s12 + (5-s13) + (5-s14) + 
+           s15 + (5-s16) + s17 + s18 + (5-s19) + s20)
+
+df_pilot1 <- as_tibble(df_pilot1) %>%
+  group_by(subjectID) %>%
+  mutate(anxiety_level = case_when(
+    sumVals < 37 ~ 'low trait anxiety',
+    sumVals >= 38 & sumVals < 44 ~ 'moderate trait anxiety',
+    sumVals > 44 ~ 'high trait anxiety'
+  ))
 
 
-#participant 1 scoring (sum)
-participant_1 <- df[ ,c(1)]
-participant_1
-total = c(sum(participant_1))
-total
-
-    
 
 
---
+++ STAI_scoring.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 15:37:07 2023
+
+@author: rmg55
+"""
+import glob
+import pandas as pd
+
+#grab all the files in the given folder that have data
+path = '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/Pilot1/'
+#r"/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/TestingData2"
+#"C:\\Users\\rmg55\\Box\\Data\\SF_Sequence2\\"
+allFiles = glob.glob(path + "*.txt")
+print('allFiles', allFiles)
+
+#initialize the final list of lists that has our data
+out_data = []
+
+# generate the column names
+colnames = ['subject'] + ['s' + str(i) for i in range(1, 21)]
+
+# add column names to out data
+out_data.append(colnames)
+print(colnames)
+
+#loop through all the files
+for file in allFiles:
+    #opens the file
+    data = open(file).readlines()
+    print('data', data)
+    
+    #grab the worker ID
+    subID = data[2].strip().split(':')[1]
+    
+    #grab the STAI data
+    STAI_data = data[5].strip().split(':')[1].split(';')
+    print(STAI_data)
+    
+    out_data.append([subID] + STAI_data)
+
+# finally, turn it into a pandas data frame
+df = pd.DataFrame(out_data)
+
+df.to_csv(path + 'STAIscores.csv')
+    
+    
+    
+    
+    
+    
+    
