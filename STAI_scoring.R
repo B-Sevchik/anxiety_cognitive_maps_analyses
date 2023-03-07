ed8f16748e1c876446ec5876a8f75999a12ee447
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-03-07 01:17:29 +0100

    update STAI scoring script
    shortened script & saved out file

--- STAI_scoring.R
+++ STAI_scoring.R
@@ -2,16 +2,17 @@
 setwd("~/Documents/GitHub/anxiety_cognitive_maps_analyses")
 library(tidyverse)
 library(dplyr)
-library(tibble)
+
+#BE SURE TO EDIT PATH NAMES EACH TIME YOU RUN SCRIPT
 
 #path references
-data_path <- '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/Pilot1/STAIscores.csv'
+data_path <- '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/Pilot1/STAIscores.csv' #EDIT PATH NAME EACH TIME
 
 #load data
 df_pilot1 <- read_csv(data_path)
 
 #manipulate df to get it to look how you want
-colnames(df_pilot1) <- c('not_included', 'subjectID', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20')
+colnames(df_pilot1) <- c('not_included', 'subjectID', paste0('s', 1:20))
 num_rows = nrow(df_pilot1)
 num_rows
 
@@ -23,33 +24,16 @@ df_pilot1 <- df_pilot1 %>%
 print(sapply(df_pilot1, class)) #debugging
 df_pilot1 <- transform(df_pilot1,s1 = as.numeric(s1))
 print(sapply(df_pilot1$s1, class)) #debugging
-df_pilot1 <- transform(df_pilot1,s2 = as.numeric(s2))
-df_pilot1 <- transform(df_pilot1,s3 = as.numeric(s3))
-df_pilot1 <- transform(df_pilot1,s4 = as.numeric(s4))
-df_pilot1 <- transform(df_pilot1,s5 = as.numeric(s5))
-df_pilot1 <- transform(df_pilot1,s6 = as.numeric(s6))
-df_pilot1 <- transform(df_pilot1,s7 = as.numeric(s7))
-df_pilot1 <- transform(df_pilot1,s8 = as.numeric(s8))
-df_pilot1 <- transform(df_pilot1,s9 = as.numeric(s9))
-df_pilot1 <- transform(df_pilot1,s10 = as.numeric(s10))
-df_pilot1 <- transform(df_pilot1,s11 = as.numeric(s11))
-df_pilot1 <- transform(df_pilot1,s12 = as.numeric(s12))
-df_pilot1 <- transform(df_pilot1,s13 = as.numeric(s13))
-df_pilot1 <- transform(df_pilot1,s14 = as.numeric(s14))
-df_pilot1 <- transform(df_pilot1,s15 = as.numeric(s15))
-df_pilot1 <- transform(df_pilot1,s16 = as.numeric(s16))
-df_pilot1 <- transform(df_pilot1,s17 = as.numeric(s17))
-df_pilot1 <- transform(df_pilot1,s18 = as.numeric(s18))
-df_pilot1 <- transform(df_pilot1,s19 = as.numeric(s19))
-df_pilot1 <- transform(df_pilot1,s20 = as.numeric(s20))
+df_pilot1 <- df_pilot1 %>%
+  mutate(across(paste0('s', 1:20), as.numeric))
 
 #sum STAI score
-df_pilot1 <- as_tibble(df_pilot1) %>% 
+df_pilot1 <- df_pilot1 %>% 
   group_by(subjectID) %>%
   mutate(sumVals = (5-s1) + s2 + (5-s3) + s4 + s5 + (5-s6) + (5-s7) + s8 + s9 + (5-s10) + s11 + s12 + (5-s13) + (5-s14) + 
            s15 + (5-s16) + s17 + s18 + (5-s19) + s20)
 
-df_pilot1 <- as_tibble(df_pilot1) %>%
+df_pilot1 <- df_pilot1 %>%
   group_by(subjectID) %>%
   mutate(anxiety_level = case_when(
     sumVals < 37 ~ 'low trait anxiety',
@@ -57,6 +41,8 @@ df_pilot1 <- as_tibble(df_pilot1) %>%
     sumVals > 44 ~ 'high trait anxiety'
   ))
 
+#save out the file
+write.csv(df_pilot1, '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/Pilot1/STAI_scores_calculated.csv') #EDIT PATH NAME EACH TIME
 
 
 
