a6e87492fce9db03ddad161e461dae510e2915f6
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-03-16 21:31:46 +0100

    updated STAI scoring
    NOT WORKING YET - but almost

---
+++ STAI_scoring_first_draft.R
@@ -0,0 +1,52 @@
+#set-up
+setwd("~/Documents/GitHub/anxiety_cognitive_maps_analyses")
+library(tidyverse)
+library(dplyr)
+
+#BE SURE TO EDIT PATH NAMES EACH TIME YOU RUN SCRIPT
+
+#path references
+data_path <- '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/Pilot1/STAIscores.csv' #EDIT PATH NAME EACH TIME
+
+#load data
+df_pilot1 <- read_csv(data_path)
+
+#manipulate df to get it to look how you want
+colnames(df_pilot1) <- c('not_included', 'subjectID', paste0('s', 1:20))
+num_rows = nrow(df_pilot1)
+num_rows
+
+df_pilot1 <- df_pilot1 %>%
+  slice(2:num_rows) %>%
+  select('subjectID', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20')
+
+#getting scores into numeric form so you can add them together later
+print(sapply(df_pilot1, class)) #debugging
+df_pilot1 <- transform(df_pilot1,s1 = as.numeric(s1))
+print(sapply(df_pilot1$s1, class)) #debugging
+df_pilot1 <- df_pilot1 %>%
+  mutate(across(paste0('s', 1:20), as.numeric))
+
+#changing non-responses (5) into mean value
+df_pilot1 <- df_pilot1 %>%
+  mutate(meanVals = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11 + s12 + s13 + s14 + s15 + s16 + s17 + s18 + s19 + s20) / 20) %>%
+  mutate(across(paste0('s', 1:20), ~ case_when(. == 5 ~ meanVals, 
+                                               TRUE ~ .)))
+
+#sum STAI score
+df_pilot1 <- df_pilot1 %>% 
+  group_by(subjectID) %>%
+  mutate(sumVals = (5-s1) + s2 + (5-s3) + s4 + s5 + (5-s6) + (5-s7) + s8 + s9 + (5-s10) + s11 + s12 + (5-s13) + (5-s14) + 
+           s15 + (5-s16) + s17 + s18 + (5-s19) + s20)
+
+df_pilot1 <- df_pilot1 %>%
+  group_by(subjectID) %>%
+  mutate(anxiety_level = case_when(
+    sumVals < 37 ~ 'low trait anxiety',
+    sumVals >= 38 & sumVals < 44 ~ 'moderate trait anxiety',
+    sumVals > 44 ~ 'high trait anxiety'
+  ))
+
+#save out the file
+write.csv(df_pilot1, '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/Pilot1/STAI_scores_calculated.csv') #EDIT PATH NAME EACH TIME
+
