ec038b6b2094a32b4105c6f0bc52ffdcb18d4dfa
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-03-22 16:00:11 +0100

    STAI scoring most updated

--- STAI_scoring.R
+++ STAI_scoring.R
@@ -27,18 +27,40 @@ print(sapply(df_pilot1$s1, class)) #debugging
 df_pilot1 <- df_pilot1 %>%
   mutate(across(paste0('s', 1:20), as.numeric))
 
-#changing non-responses (5) into mean value
+
+
+
+#changing non-responses (5) into NaN
 df_pilot1 <- df_pilot1 %>%
-  mutate(meanVals = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11 + s12 + s13 + s14 + s15 + s16 + s17 + s18 + s19 + s20) / 20) %>%
-  mutate(across(paste0('s', 1:20), ~ case_when(. == 5 ~ meanVals, 
-                                              TRUE ~ .)))
+  mutate(across(paste0('s', 1:20), ~ case_when(. == 5 ~ 0, 
+                                               TRUE ~ .)))
+
+#reverse score what needs to be reverse scored
+df_pilot1 <- df_pilot1 %>%
+  group_by(subjectID) %>%
+  mutate(s1 = ifelse(s1== 0, 0, 5 - s1),
+         s3 = ifelse(s3== 0, 0, 5 - s3),
+         s6 = ifelse(s6== 0, 0, 5 - s6),
+         s7 = ifelse(s7== 0, 0, 5 - s7),
+         s10 = ifelse(s10== 0, 0, 5 - s10),
+         s13 = ifelse(s13== 0, 0, 5 - s13),
+         s14 = ifelse(s14== 0, 0, 5 - s14),
+         s16 = ifelse(s16== 0, 0, 5 - s16),
+         s19 = ifelse(s19== 0, 0, 5 - s19))
+        
+
+#get the mean & replace NaNs(0s) w the mean 
+df_pilot1 <- df_pilot1 %>%
+  mutate(meanVals = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11 + s12 + s13 + s14 + s15 + s16 + s17 + s18 + s19 + s20)/ 20) %>%
+  mutate(across(paste0('s', 1:20), ~ case_when(. == 0 ~ meanVals, 
+                                               TRUE ~ .)))
 
 #sum STAI score
-df_pilot1 <- df_pilot1 %>% 
+df_pilot1 <- df_pilot1 %>%
   group_by(subjectID) %>%
-  mutate(sumVals = (5-s1) + s2 + (5-s3) + s4 + s5 + (5-s6) + (5-s7) + s8 + s9 + (5-s10) + s11 + s12 + (5-s13) + (5-s14) + 
-           s15 + (5-s16) + s17 + s18 + (5-s19) + s20)
+  mutate(sumVals = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11 + s12 + s13 + s14 + s15 + s16 + s17 + s18 + s19 + s20)
 
+#classify anxiety levels based on STAI sum
 df_pilot1 <- df_pilot1 %>%
   group_by(subjectID) %>%
   mutate(anxiety_level = case_when(
