620820f505c9c86e5196dbdf7b10c0a72ec71879
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-06-13 13:01:38 -0400

    added another plot for drop event including accuracy
    filtered for out of the trials where the first drag & drop the participant does is accurate

--- analyze_drop.R
+++ analyze_drop.R
@@ -28,3 +28,27 @@ percentage_drop_threat_df <- drop_combined_trials_df %>%
     percentage_neutral = sum(dragThreat == "neutral") / n() * 100
   )
 percentage_drop_threat_df
+
+
+
+#ACCURACY INCLUDED
+
+#filter where the first image participants dragged & dropped was accurate/they put it in the right spot, and observe based off of those trials
+
+# group by subject and trial count, filter for trialAttempt == 1 and grab the first row, then filter for dragAcc == 1
+drop_combined_trials_acc_df <- drop_df %>%
+  group_by(subject, trialCount) %>%
+  filter(trialAttempt == 1) %>%
+  slice(1) %>%
+  ungroup() %>%
+  filter(dragAcc == 1)
+drop_combined_trials_acc_df
+
+# calculate percentage of threat being first vs. neutral being first drag out of all the trials for each subject WHERE THEY WERE ACCURATE
+percentage_drop_threat_acc_df <- drop_combined_trials_acc_df %>%
+  group_by(subject) %>%
+  summarize(
+    percentage_threat = sum(dragThreat == "threat") / n() * 100,
+    percentage_neutral = sum(dragThreat == "neutral") / n() * 100
+  )
+percentage_drop_threat_acc_df
